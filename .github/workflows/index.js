b0abbd185d3dac5abf0eebc0aff3641cf9bbac60
Amaan Saeed <asaeed@threekit.com>
2022-02-19 04:33:16 -0500

    initial commit

---
+++ .github/workflows/index.js
@@ -0,0 +1,34 @@
+const core = require("@actions/core");
+// const github = require("@actions/github");
+const YAML = require("yaml");
+const fs = require("fs");
+
+function prepHosts(projectName, isDev) {
+  if (!isDev) return [`${projectName}.3kit.com`];
+  return ["main", "prod", "dev", "staging"].reduce((output, branch) => {
+    output.push(`${projectName}.${branch}.3kit.com`);
+    return output;
+  }, []);
+}
+
+function getPorterConfig(projectName, isDev) {
+  const porterConfig = {
+    ingress: {
+      custom_domain: true,
+      hosts: prepHosts(projectName, isDev),
+    },
+  };
+  return YAML.stringify(porterConfig);
+}
+
+try {
+  // `project-name` input defined in action metadata file
+  const projectName = core.getInput("project-name");
+  const isDev = core.getInput("env") === "dev";
+
+  const config = getPorterConfig(projectName, isDev);
+
+  fs.writeFileSync("./porter.yml", JSON.stringify(config));
+} catch (error) {
+  core.setFailed(error.message);
+}
